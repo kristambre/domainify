35acd1e0b08ea935b899827e0ab18ec9fbda4041
Kris Tambre <kris.tambre@gmail.com>
2017-11-23 21:35:45 +0200

    Added moving up one subpath at a time

--- domainify/background.js
+++ domainify/background.js
@@ -6,22 +6,55 @@ function parseHost(host) {
     return "//"+host;
 }
 
+function parsePath(path) {
+    return path.split("/");
+}
+
 browser.commands.onCommand.addListener(function(command) {
-    if(command == "go-to-root") {
-        let tab = browser.tabs.query({ currentWindow: true, active: true });
-        
-        tab.then(function(tabs) {
-            for (let tab of tabs) {
-                browser.tabs
-                .sendMessage(tab.id, {message: "get-url"})
-                .then(response => {
-                    let parsedURL = response.protocol // appends ':' by default
-                                    + parseHost(response.host) // prepends '//'
-                                    + parsePort(response.port); // prepends ':' if port is visible
-                    
-                    browser.tabs.update({url: parsedURL});
-                });
+    let tab = browser.tabs.query({ currentWindow: true, active: true });
+
+    tab.then(function(tabs) {
+                for (let tab of tabs) {
+                    browser.tabs
+                    .sendMessage(tab.id, {message: "get-url"})
+                    .then(response => {
+                        updateTab(response, command);
+                    });
+                }
+            });
+});
+
+function updateTab(urlData, command) {
+    let parsedURL = '';
+
+    switch (command) {
+        case "go-to-root":        
+            parsedURL = urlData.protocol // appends ':' by default
+                                        + parseHost(urlData.host) // prepends '//'
+                                        + parsePort(urlData.port); // prepends ':' if port is visible
+            break;
+        case "go-up":
+            parsedURL = urlData.protocol // appends ':' by default
+                                        + parseHost(urlData.host) // prepends '//'
+                                        + parsePort(urlData.port); // prepends ':' if port is visible
+
+            let parsedPathArray = parsePath(urlData.path);
+
+            for (let i = 0; i < parsedPathArray.length - 1; i++) {
+                let path = parsedPathArray[i];
+                
+                if (path != '') {
+                    parsedURL = parsedURL.concat("/").concat(path);
+                }
             }
-        });
-    }
+
+            break;
+        }
+
+    browser.tabs.update({url: parsedURL});
+
+}
+
+browser.browserAction.onClicked.addListener(function() {
+    browser.runtime.openOptionsPage();
 });
--- domainify/options/options.js
+++ domainify/options/options.js
@@ -1,21 +1,3 @@
-function saveOptions(e) {
-  let color = document.querySelector("#colour").value;
-  browser.storage.sync.set({
-    colour: color
-  });
-
-  document.querySelector("#current-color").innerText = color;
-  window.location.reload();
-
-  e.preventDefault();
-}
-
-function restoreOptions() {
-  var gettingItem = browser.storage.sync.get('colour');
-  gettingItem.then((res) => {
-    document.querySelector("#current-colour").innerText = res.colour || 'none';
-  });
-}
-
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.getElementById("go_to_root").addEventListener('keydown', (event) => {
+    
+});
